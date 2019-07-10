5acdd16b3b2224b61ebfabcf06ee62009c927657
Your Name <you@example.com>
2019-07-10 15:45:22 -0300

    cambios esteticos

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.main')
 @section('content')
 {{-- {{Auth::user()}} --}}
-  <div class="container">
-      <div class="">
+  <div class="container p-3">
+    <div class="row align-items-center ">
+      <div class="col sm-6 md-6 lg-6 d-flex justify-content-center ">
         <a href="profile/edit">Edit profile</a> <br>
         <img src="/storage/{{Auth::user()->avatar}}" alt="avatar" class="_imgAvatar">
-        <h2>{{Auth::user()->first_name}} {{Auth::user()->last_name}}</h2>
+
+      </div>
+      <div class="col sm-6 md-6 lg-6">
         <ul>
+          <li><h2>{{Auth::user()->first_name}} {{Auth::user()->last_name}}</h2></li>
           <li>Birth Date:{{Auth::user()->birth_date}}</li>
           <li>Country:{{Auth::user()->country}}</li>
           </ul>
-        </div>
-      <a class="btn btn-primary" href="profile/edit">Edit profile</a> <br>
+          <a class="btn btn-primary" href="profile/edit">Edit profile</a> <br>
+      </div>
+    </div>
   </div>
 @endsection()
